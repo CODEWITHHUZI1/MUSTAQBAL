75fa3c1859a39d227055b72dafb84e989330bf2f
CODEWITHHUZI1 <m.huzaifa.khan471@gmail.com>
2026-01-24 21:16:24 +0500

    Update app (5).py

--- app (5).py
+++ app (5).py
@@ -249,8 +249,7 @@ def render_library():
 def render_about():
     st.header("ℹ️ About Alpha Apex")
     st.info("Version: 2.1.0 | Engine: Google Gemini 1.5")
-    st.table([{"Name": "Saim Ahmed", "Designation": "Lead Full Stack Developer", "Email": }, {"Name": "Huzaifa Khan", "Designation": "AI System Architect", "Email":"m.huzaifa.khan471@gmail.com"},{"Name": "Ibrahim Sohail","Designation":"Presentation Lead","Email":"ibrahimsohailkhan10@gmail.com"},{"Name":"Daniyal Faraz", "Designation": "Debugger","Email": "daniyalfarazkhan2012@gmail.com"},{"Name": "Muhammad Mustafa Khan", "Designation": "Prompt Engineer" , "Email":'muhammadmustafakhan430@gmail.com"}
-              ])
+    st.table([{"Name": "Saim Ahmed", "Designation": "Lead Full Stack Developer", "Email": }, {"Name": "Huzaifa Khan", "Designation": "AI System Architect", "Email":"m.huzaifa.khan471@gmail.com"},{"Name": "Ibrahim Sohail","Designation":"Presentation Lead","Email":"ibrahimsohailkhan10@gmail.com"},{"Name":"Daniyal Faraz", "Designation": "Debugger","Email": "daniyalfarazkhan2012@gmail.com"},{"Name": "Muhammad Mustafa Khan", "Designation": "Prompt Engineer" , "Email":'muhammadmustafakhan430@gmail.com"}])
 
 def render_login_portal():
     st.title("⚖️ Alpha Apex Secure Entrance")
@@ -304,5 +303,6 @@ else:
     if navigation_selection == "Consultation Chambers": render_chambers()
     elif navigation_selection == "Digital Library": render_library()
     else: render_about()
+
 
 
